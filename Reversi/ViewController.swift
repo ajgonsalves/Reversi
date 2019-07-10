6d7ad9336d1f4792394989fd98208504b72fcef1
ajgonsalves <gonsalves.alexander@gmail.com>
2019-07-10 00:44:43 -0700

    Initial Commit

---
+++ Reversi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reversi
+//
+//  Created by Alexander on 7/10/19.
+//  Copyright © 2019 gonsalves.alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
